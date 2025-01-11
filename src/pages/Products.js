61498026592b30b71b31d6d853fcaed888fa2d30
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 18:46:49 +0500

    show products array on /products + navigate and show info on single product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import products from '../data'
 
 const Products = () => {
   return (
     <>
       <section className='section'>
         <h2>products</h2>
-        <Link to='/' className="btn">Home</Link>
-        <Link to='/about' className="btn">About</Link>
+        
+        <div className="products">
+          {products.map((p) =>
+            <article key={p.id}>
+              <h5>{p.name}</h5>
+              <Link to={`/products/${p.id}`}>More Info</Link>
+          </article>)}
+        </div>
       </section>
     </>
   );
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,15 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const { productID } = useParams()
+  const product = products.find((p) => p.id === productID)
+  const {image, name} = product
   
   return (
     <section className='section product'>
-      <h2>single product - {productID}</h2>
+      <img src={image} alt={name} />
+      <h2>{name} - {productID}</h2>
       <Link to='/products'>Back to Products</Link>
     </section>
   );
