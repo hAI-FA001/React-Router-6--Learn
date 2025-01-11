9563e247b32b0f0729bd4c2b880f9b3f2237d8e6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 17:50:45 +0500

    using components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
 
 function App() {
   return <BrowserRouter>
     <Routes>
-      <Route path='/' element={<div>Home</div>} />
-      <Route path='testing' element={<div>Testing route</div>} />
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About/>} />
+      <Route path='products' element={<Products/>} />
     </Routes>
   </BrowserRouter>
 }
