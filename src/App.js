864936b4961952bad643c6899c7d2e98c8caceff
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 18:53:58 +0500

    navigate programmatically with useNavigation in login

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import About from './pages/About'
 import Products from './pages/Products'
 import SingleProduct from './pages/SingleProduct'
 import Error from './pages/Error'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 import SharedLayout from "./pages/SharedLayout";
+import { useState } from "react";
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SharedLayout />} >
-        <Route index element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='products' element={<Products />} />
-        <Route path='products/:productID' element={<SingleProduct/>} />
-        <Route path='*' element={<Error />} />
-      </Route>
-    </Routes>
-    <footer>Footer</footer>
-  </BrowserRouter>
+  const [user, setUser] = useState(null)
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />} >
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:productID' element={<SingleProduct />} />
+          <Route path='login' element={<Login setUser={setUser} />} />
+          <Route path='dashboard' element={<Dashboard user={user} />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+      <footer>Footer</footer>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -6,10 +6,12 @@ const StyledNavbar = () => {
           <NavLink to='/' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Home</NavLink>
           <NavLink to='/about' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>About</NavLink>
           <NavLink to='/products' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Products</NavLink>
+          <NavLink to='/login' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Login</NavLink>
           
           <NavLink to='/' className={({ isActive }) => isActive? 'link active' : 'link'}>Home</NavLink>
           <NavLink to='/about' className={({isActive}) => isActive? 'link active' : 'link'}>About</NavLink>
           <NavLink to='/products' className={({isActive}) => isActive? 'link active' : 'link'}>Products</NavLink>
+          <NavLink to='/login' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Login</NavLink>
     </nav>
   )
 }
