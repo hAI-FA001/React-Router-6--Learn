e73237ebb965f49e409ff5415d816e4ba60d7663
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 18:33:35 +0500

    NavLink for styling currently active url/link

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom"
+
+const StyledNavbar = () => {
+  return (
+      <nav className="navbar">
+          <NavLink to='/' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Home</NavLink>
+          <NavLink to='/about' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>About</NavLink>
+          <NavLink to='/products' style={({isActive}) => {return {color: (isActive? 'red' : 'gray')}}}>Products</NavLink>
+          
+          <NavLink to='/' className={({ isActive }) => isActive? 'link active' : 'link'}>Home</NavLink>
+          <NavLink to='/about' className={({isActive}) => isActive? 'link active' : 'link'}>About</NavLink>
+          <NavLink to='/products' className={({isActive}) => isActive? 'link active' : 'link'}>Products</NavLink>
+    </nav>
+  )
+}
+
+export default StyledNavbar
