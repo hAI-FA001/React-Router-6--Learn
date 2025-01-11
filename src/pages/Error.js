f4f0bfcf98f09a0a21f38cfad9455cb168f28ed8
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 17:59:11 +0500

    update error page, add Link to home

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>Error 404</h2>
+      <p>Page Not Found</p>
+      <Link to='/' className="btn">Go Back To Home</Link>
     </section>
   );
 };
