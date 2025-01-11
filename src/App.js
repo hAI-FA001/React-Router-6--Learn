a142421cf5885e109faa6234eb68ab752949cbe2
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-11 17:59:01 +0500

    handle error route, path=*

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
+import Error from './pages/Error'
 
 function App() {
   return <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='about' element={<About/>} />
-      <Route path='products' element={<Products/>} />
+      <Route path='products' element={<Products />} />
+      <Route path='*' element={<Error/>} />
     </Routes>
   </BrowserRouter>
 }
